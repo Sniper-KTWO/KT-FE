151e13feca3608ce96c983582a827c9ac5ccadb5
Hyewon Yoon <iey704@gachon.ac.kr>
2024-10-17 15:21:49 +0900

    feat: 상단바 및 이미지 추가

--- src/app/(yogo)/(home)/page.js
+++ src/app/(yogo)/(home)/page.js
@@ -1,7 +1,14 @@
+import { Button } from "@nextui-org/react";
+import Image from "next/image";
+
 export default function home() {
   return (
     <div>
-      <h1>Welcome to K-Two</h1>
+      <Image src="/images/1-1.png" alt="1-1" width={390} height={844} />
+      <Image src="/images/1-2.png" alt="1-2" width={390} height={844} />
+      <Image src="/images/1-3.png" alt="1-3" width={390} height={844} />
+      <Image src="/images/1-4.png" alt="1-4" width={390} height={844} />
+      <Image src="/images/1-5.png" alt="1-5" width={390} height={844} />
     </div>
   );
 }
--- src/app/(yogo)/layout.js
+++ src/app/(yogo)/layout.js
@@ -1,15 +1,45 @@
 import Image from "next/image";
+import { Icon } from "@iconify/react";
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+} from "@nextui-org/react";
 
-export default function layout({ children }) {
+export default function Layout({ children }) {
   return (
     <div>
-      <Image src="/images/kt_logo.png" alt="kt_logo" width={30} height={25} />
-      <Image
-        src="/images/shop_logo.png"
-        alt="shop_logo"
-        width={60}
-        height={25}
-      />
+      <Navbar isBordered>
+        <NavbarBrand>
+          <Link href="/">
+            <Image
+              src="/images/kt_logo.png"
+              alt="kt_logo"
+              width={30}
+              height={25}
+            />
+          </Link>
+        </NavbarBrand>
+        <NavbarContent justify="end">
+          <NavbarItem>
+            <Button variant="light">
+              <Icon
+                icon="solar:headphones-round-outline"
+                width={40}
+                height={40}
+              />
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <Button variant="light">
+              <Icon icon="solar:hamburger-menu-linear" width={40} height={40} />
+            </Button>
+          </NavbarItem>
+        </NavbarContent>
+      </Navbar>
 
       {children}
     </div>
