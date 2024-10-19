1e66b11a8b962a402ee3b9e154d56b148004ed74
yelynnn <lynn1833@ewhain.net>
2024-10-20 00:31:19 +0900

    feat: 이미지 슬라이더 구현

--- src/app/(yogo)/yogoBrand/page.js
+++ src/app/(yogo)/yogoBrand/page.js
@@ -1,8 +1,9 @@
+import BrandSlide from '@/components/slider/brand_slider';
+
 export default function yogoBrand() {
   return (
-    <div>
-      <h1>요고 브랜드 스토리</h1>
+    <div className="homeStyle">
+      <BrandSlide />
     </div>
   );
 }
-// 요고 브랜드 스토리
