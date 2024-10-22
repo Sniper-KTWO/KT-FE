8999ace045017230095ec89cbe42318b7e9e0e39
Hyewon Yoon <iey704@gachon.ac.kr>
2024-10-23 00:58:02 +0900

    feat: 요고 요금제 변경 페이지 전체

--- src/app/(yogo)/yogoChange/page.js
+++ src/app/(yogo)/yogoChange/page.js
@@ -1,12 +1,10 @@
-import ChangeMenu from "@/components/change/change_menu";
+import { Card } from "@nextui-org/react";
 import CustomTab from "@/components/tab/customTab";
-
-import { Button } from "@nextui-org/react";
 import "./styles/page.css";
+
 export default function yogoChange() {
   return (
     <div className="homeStyle">
-      <ChangeMenu />
       <CustomTab />
     </div>
   );
