ab7322b67a36499926199e0852d75da136d855b4
yelynnn <lynn1833@ewhain.net>
2024-10-23 14:42:18 +0900

    feat: 변경하기 버튼 구현

---
+++ src/components/modal/change_modal.js
@@ -0,0 +1,41 @@
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import { Icon } from '@iconify/react';
+import './styles/change_modal.css';
+
+function ChangeModal() {
+  const dialogRef = useRef(null);
+
+  const showModal = () => {
+    dialogRef.current?.showModal();
+  };
+
+  const closeModal = () => {
+    dialogRef.current?.close();
+  };
+
+  return (
+    <div>
+      <button onClick={showModal}>변경하기</button>
+
+      <dialog ref={dialogRef}>
+        <Icon
+          className="close"
+          onClick={closeModal}
+          icon="material-symbols-light:close"
+          width={20}
+          height={20}
+        />
+        <div className="change">
+          <Icon icon="la:check-circle" width={30} height={40} />
+          <div className="alert">요금제가 변경되었습니다.</div>
+          <button className="check" onClick={closeModal}>
+            확인
+          </button>
+        </div>
+      </dialog>
+    </div>
+  );
+}
+
+export default ChangeModal;
