d648abce11d3f85567e714b2cc4609ee93a3172e
yelynnn <lynn1833@ewhain.net>
2024-10-23 14:43:01 +0900

    feat: 대표화면에 버튼 추가

--- src/app/(yogo)/yogoChange/page.js
+++ src/app/(yogo)/yogoChange/page.js
@@ -1,11 +1,21 @@
-import ChangeMenu from '@/components/change/change_menu';
+'use client';
+import React from 'react';
+import ChangeMenu from '../../../components/change/change_menu';
 import { Button } from '@nextui-org/react';
 import './styles/page.css';
+import CompareModal from '../../../components/modal/compare_modal';
+import ChangeModal from '../../../components/modal/change_modal';
+import { CompareProvider } from '../../../components/modal/CompareContext';
 
-export default function yogoChange() {
+export default function YogoChange() {
   return (
-    <div className="homeStyle">
-      <ChangeMenu />
-    </div>
+    <CompareProvider>
+      <div className="homeStyle">
+        <ChangeMenu />
+        <CompareModal />
+        <CompareModal />
+        <ChangeModal />
+      </div>
+    </CompareProvider>
   );
 }
