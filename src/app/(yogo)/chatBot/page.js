b5077e5bad3dc73cdaaa221884e4ff397487b30d
jiyoon kim <yunipo0803@gmail.com>
2024-10-27 23:52:44 +0900

    feat: 챗봇파일 수정

--- src/app/(yogo)/chatBot/page.js
+++ src/app/(yogo)/chatBot/page.js
@@ -1,23 +1,32 @@
 "use client";
-import styles from "./styles/page.module.css";
+import styles from "./styles/chatBot.module.css";
+import Image from "next/image";
 import { Input, Button } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
-import SendButton from "@/components/send";
 
-export default function chatBot() {
+export default function ChatBot() {
   return (
     <div className={styles.page}>
       <div className={styles.inputContainer}>
         <Input
           className={styles.inputField}
           clearable
-          contentRightStyling={false}
+          contentRight={
+            <div
+              className={styles.iconContainer}
+              onClick={() => console.log("전송 버튼 누름")}
+            >
+              <Image
+                src="/images/send-icon.png"
+                alt="메시지 전송 버튼 아이콘"
+                width={30}
+                height={31}
+                className={styles.sendIcon}
+              />
+            </div>
+          }
           placeholder="요금제에 관해 궁금한 점을 물어보세요!"
-          contentRight={<SendButton />}
         />
-        {/* <Button isIconOnly variant="light" size="sm">
-          <Icon icon="bi:send-fill" width={30} height={30} />
-        </Button> */}
       </div>
     </div>
   );
