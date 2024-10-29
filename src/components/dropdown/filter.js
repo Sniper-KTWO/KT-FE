6bfbe405e2a144c86d64dda5f763f7145f5ad7f3
Hyewon Yoon <iey704@gachon.ac.kr>
2024-10-29 21:25:27 +0900

    feat: 필터 모달 구현

--- src/components/dropdown/filter.js
+++ src/components/dropdown/filter.js
@@ -8,9 +8,11 @@ import {
 } from "@nextui-org/react";
 import { Icon } from "@iconify/react";
 import styles from "./styles/menu.module.css";
+import FilterModal from "../modal/filter_modal";
 
-export default function Filter({ onFilterChange }) {
+export default function Filter({ onFilterChange, onSearchChange }) {
   const [selected, setSelected] = useState(new Set(["전체"]));
+  const [filters, setFilters] = useState([]);
 
   const selectedValue = React.useMemo(
     () => Array.from(selected).join(", ").replaceAll("_", " "),
@@ -20,6 +22,15 @@ export default function Filter({ onFilterChange }) {
   const handleFilterChange = (keys) => {
     setSelected(keys);
     onFilterChange(Array.from(keys).join(", ")); // 부모 컴포넌트로 선택된 값 전달
+  };
+
+  // 필터 모달에서 받은 값을 처리하는 함수
+  const handleSearchChange = ({ minFee, maxFee, network }) => {
+    console.log("Filter에서 받은 값:", { minFee, maxFee, network });
+    setFilters({ minFee, maxFee, network });
+    if (onSearchChange) {
+      onSearchChange({ minFee, maxFee, network });
+    }
   };
 
   return (
@@ -49,10 +60,11 @@ export default function Filter({ onFilterChange }) {
 
       {/* 필터 2 */}
       <div style={{ paddingLeft: "10px" }}>
-        <Button auto light className={styles.filter}>
+        {/* <Button auto light className={styles.filter}>
           <Icon icon="rivet-icons:filter" className={styles.filterIcon} />
           필터
-        </Button>
+        </Button> */}
+        <FilterModal onSearchChange={handleSearchChange} />
       </div>
     </>
   );
