e8beec92f3fb743a2b38624a5d38d926bb2feba3
yelynnn <lynn1833@ewhain.net>
2024-10-30 23:36:20 +0900

    fix: 필터링 중복/전체 개수 오류 해결

--- src/components/modal/filter_modal.js
+++ src/components/modal/filter_modal.js
@@ -1,11 +1,11 @@
-"use client";
-import React, { useState, useRef } from "react";
-import "./styles/filter_modal.css";
-import { Button, Divider } from "@nextui-org/react";
-import Image from "next/image";
-import PriceSlider from "../slider/price_slider";
-import styles from "../dropdown/styles/menu.module.css";
-import { Icon } from "@iconify/react";
+'use client';
+import React, { useState, useRef } from 'react';
+import './styles/filter_modal.css';
+import { Button, Divider } from '@nextui-org/react';
+import Image from 'next/image';
+import PriceSlider from '../slider/price_slider';
+import styles from '../dropdown/styles/menu.module.css';
+import { Icon } from '@iconify/react';
 
 function FilterModal({ onSearchChange }) {
   const dialogRef = useRef(null);
@@ -14,21 +14,31 @@ function FilterModal({ onSearchChange }) {
   const [isLGActive, setIsLGActive] = useState(false);
   const [minFee, setMinFee] = useState(0);
   const [maxFee, setMaxFee] = useState(70000);
-
-  // const onSearchChange = ({ minFee, maxFee, network }) => {
-  //   setLoading(true);
-  // };
+  const [selectedNetWorks, setSelectedNetWorks] = useState([]);
 
   const handleKTClick = () => {
     setIsKTActive((prevState) => !prevState);
+    setSelectedNetWorks((prev) =>
+      prev.includes('KT') ? prev.filter((net) => net !== 'KT') : [...prev, 'KT']
+    );
   };
 
   const handleSKTClick = () => {
     setIsSKTActive((prevState) => !prevState);
+    setSelectedNetWorks((prev) =>
+      prev.includes('SKT')
+        ? prev.filter((net) => net !== 'SKT')
+        : [...prev, 'SKT']
+    );
   };
 
   const handleLGClick = () => {
     setIsLGActive((prevState) => !prevState);
+    setSelectedNetWorks((prev) =>
+      prev.includes('LGU')
+        ? prev.filter((net) => net !== 'LGU')
+        : [...prev, 'LGU']
+    );
   };
 
   const showModal = () => {
@@ -41,16 +51,11 @@ function FilterModal({ onSearchChange }) {
     setIsLGActive(false);
     setMinFee(0);
     setMaxFee(70000);
+    setSelectedNetWorks([]);
     dialogRef.current?.close();
   };
 
   const handleConfirm = () => {
-    const selectedNetWorks = [];
-
-    if (isKTActive) selectedNetWorks.push("KT");
-    if (isSKTActive) selectedNetWorks.push("SKT");
-    if (isLGActive) selectedNetWorks.push("LG");
-
     if (onSearchChange) {
       onSearchChange({
         minFee,
@@ -59,15 +64,15 @@ function FilterModal({ onSearchChange }) {
       });
     }
 
-    console.log("min:", minFee);
-    console.log("max:", maxFee);
-    console.log("network:", selectedNetWorks);
+    console.log('min:', minFee);
+    console.log('max:', maxFee);
+    console.log('network:', selectedNetWorks);
 
     dialogRef.current?.close();
   };
 
   return (
-    <div style={{ height: "auto" }}>
+    <div style={{ height: 'auto' }}>
       <Button auto light className={styles.filter} onClick={showModal}>
         <Icon icon="rivet-icons:filter" className={styles.filterIcon} />
         필터
@@ -91,51 +96,51 @@ function FilterModal({ onSearchChange }) {
             className="logo"
             onClick={handleKTClick}
             style={{
-              backgroundColor: !isKTActive ? "#f5f5f5" : "#86e8d1",
+              backgroundColor: !isKTActive ? '#f5f5f5' : '#86e8d1',
             }}
           >
             <Image
               className="logoImg"
-              src={"/images/kt_logo.png"}
+              src={'/images/kt_logo.png'}
               alt="kt"
               width={15}
               height={15}
             />
-            <span style={{ marginLeft: "-45px" }}>KT</span>
+            <span style={{ marginLeft: '-45px' }}>KT</span>
           </button>
           <button
             aria-label="Network Selection Button2"
             className="logo"
             onClick={handleSKTClick}
             style={{
-              backgroundColor: !isSKTActive ? "#f5f5f5" : "#86e8d1",
+              backgroundColor: !isSKTActive ? '#f5f5f5' : '#86e8d1',
             }}
           >
             <Image
               className="logoImg"
-              src={"/images/skt_logo.png"}
+              src={'/images/skt_logo.png'}
               alt="skt"
               width={17}
               height={17}
             />
-            <span style={{ marginLeft: "-45px" }}>SKT</span>
+            <span style={{ marginLeft: '-45px' }}>SKT</span>
           </button>
           <button
             aria-label="Network Selection Button3"
             className="logo"
             onClick={handleLGClick}
             style={{
-              backgroundColor: !isLGActive ? "#f5f5f5" : "#86e8d1",
+              backgroundColor: !isLGActive ? '#f5f5f5' : '#86e8d1',
             }}
           >
             <Image
               className="logoImg"
-              src={"/images/lgu_logo.png"}
+              src={'/images/lgu_logo.png'}
               alt="LG"
               width={17}
               height={17}
             />
-            <span style={{ marginLeft: "-45px" }}>LG</span>
+            <span style={{ marginLeft: '-45px' }}>LG</span>
           </button>
         </div>
         <div className="buttons">
