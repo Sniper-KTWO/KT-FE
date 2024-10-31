5189f366aa17b768a641ce21c42e0f6d16e38d91
jiyoon kim <yunipo0803@gmail.com>
2024-10-31 16:10:23 +0900

    feat: 챗봇 최종 1차 구현 완료

--- src/app/(yogo)/chatBot/page.js
+++ src/app/(yogo)/chatBot/page.js
@@ -14,7 +14,16 @@ export default function ChatBot() {
   const [message, setMessage] = useState("");
   const [chatMessages, setChatMessages] = useState([]);
   const hasLoaded = useRef(false);
-  const buttonTexts = useRef([]); // 버튼 텍스트와 액션 매핑 저장
+  const buttonTexts = useRef([]);
+
+  const formatDate = (date) => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "오후" : "오전";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    return `${ampm} ${formattedHours}:${formattedMinutes}`;
+  };
 
   useEffect(() => {
     if (hasLoaded.current) return;
@@ -27,28 +36,32 @@ export default function ChatBot() {
           {
             id: `${initialMessageData.id}-${Date.now()}-${Math.random()}`,
             sender: initialMessageData.sender,
+            time: formatDate(new Date()),
             content: (
-              <div>
-                <p className={styles.senderLabel}>KT 챗봇</p>
-                <div className={styles.bubbleContainer1}>
-                  <Image
-                    src="/images/chatbot-yogo.png"
-                    alt="요고 다이렉트 이미지"
-                    width={218}
-                    height={86}
-                    className={styles.image}
-                  />
-                  <div className={styles.textbox}>
-                    <p className={styles.title}>{initialMessageData.text1}</p>
-                    <p className={styles.text2}>{initialMessageData.text2}</p>
-                    <p className={styles.text3}>{initialMessageData.text3}</p>
-                    <p className={styles.text4}>{initialMessageData.text4}</p>
-                    <p className={styles.text5}>{initialMessageData.text5}</p>
-                    <p className={styles.text6}>{initialMessageData.text6}</p>
-                    <p className={styles.text7}>{initialMessageData.text7}</p>
-                    <p className={styles.text8}>{initialMessageData.text8}</p>
+              <div className={styles.bubbleContainer_bot}>
+                <div className={styles.botTextBox}>
+                  <p className={styles.senderLabel}>KT 챗봇</p>
+                  <div className={styles.bubbleContainer1}>
+                    <Image
+                      src="/images/chatbot-yogo.png"
+                      alt="요고 다이렉트 이미지"
+                      width={218}
+                      height={86}
+                      className={styles.image}
+                    />
+                    <div className={styles.textbox}>
+                      <p className={styles.title}>{initialMessageData.text1}</p>
+                      <p className={styles.text2}>{initialMessageData.text2}</p>
+                      <p className={styles.text3}>{initialMessageData.text3}</p>
+                      <p className={styles.text4}>{initialMessageData.text4}</p>
+                      <p className={styles.text5}>{initialMessageData.text5}</p>
+                      <p className={styles.text6}>{initialMessageData.text6}</p>
+                      <p className={styles.text7}>{initialMessageData.text7}</p>
+                      <p className={styles.text8}>{initialMessageData.text8}</p>
+                    </div>
                   </div>
                 </div>
+                <p className={styles.timeStampBot}>{formatDate(new Date())}</p>
               </div>
             ),
           },
@@ -74,30 +87,36 @@ export default function ChatBot() {
             secondResponseData.buttons.find((button) => button.text === text)
           );
 
-          buttonTexts.current = orderedButtons; // 버튼 텍스트와 액션 매핑 저장
+          buttonTexts.current = orderedButtons;
 
           setChatMessages((prev) => [
             ...prev,
             {
               id: `${secondResponseData.id}-${Date.now()}-${Math.random()}`,
               sender: secondResponseData.sender,
+              time: formatDate(new Date()),
               content: (
-                <div>
-                  <div className={styles.bubbleContainer2}>
-                    <div className={styles.buttonGroup}>
-                      {orderedButtons.map((button, index) => (
-                        <button
-                          key={`${button.action}-${index}`}
-                          className={styles.button}
-                          onClick={() =>
-                            handleButtonClick(button.action, button.text)
-                          }
-                        >
-                          {button.text}
-                        </button>
-                      ))}
+                <div className={styles.bubbleContainer_bot}>
+                  <div className={styles.botTextBox}>
+                    <div className={styles.bubbleContainer2}>
+                      <div className={styles.buttonGroup}>
+                        {orderedButtons.map((button, index) => (
+                          <button
+                            key={`${button.action}-${index}`}
+                            className={styles.button}
+                            onClick={() =>
+                              handleButtonClick(button.action, button.text)
+                            }
+                          >
+                            {button.text}
+                          </button>
+                        ))}
+                      </div>
                     </div>
                   </div>
+                  <p className={styles.timeStampBot}>
+                    {formatDate(new Date())}
+                  </p>
                 </div>
               ),
             },
@@ -116,17 +135,17 @@ export default function ChatBot() {
       );
 
       if (buttonData) {
-        // 인풋에 입력된 텍스트가 버튼 텍스트와 일치하는 경우
         handleButtonClick(buttonData.action, buttonData.text);
       } else {
-        // 버튼 텍스트 외의 입력에 대한 기본 응답 추가
         setChatMessages((prev) => [
           ...prev,
           {
             id: `${Date.now()}-${Math.random()}`,
             sender: "user",
+            time: formatDate(new Date()),
             content: (
               <div className={styles.bubbleContainer_user}>
+                <p className={styles.timeStampUser}>{formatDate(new Date())}</p>
                 <p className={styles.userText}>{message}</p>
               </div>
             ),
@@ -134,41 +153,45 @@ export default function ChatBot() {
           {
             id: `default-response-${Date.now()}`,
             sender: "bot",
+            time: formatDate(new Date()),
             content: (
-              <div>
-                <p className={styles.senderLabel}>KT 챗봇</p>
-                <div className={styles.bubbleContainer3}>
-                  <p className={styles.yogoPlan_content}>
-                    죄송해요, 아직 준비된 답변만 제공해드릴 수 있어요. 버튼을
-                    눌러보시거나 원하는 요금제 이름을 입력해 주세요.
-                  </p>
+              <div className={styles.bubbleContainer_bot}>
+                <div className={styles.botTextBox}>
+                  <p className={styles.senderLabel}>KT 챗봇</p>
+                  <div className={styles.bubbleContainer3}>
+                    <p className={styles.yogoPlan_content}>
+                      죄송해요, 아직 준비된 답변만 제공해드릴 수 있어요. 버튼을
+                      눌러보시거나 원하는 요금제 이름을 입력해 주세요.
+                    </p>
+                  </div>
                 </div>
+                <p className={styles.timeStampBot}>{formatDate(new Date())}</p>
               </div>
             ),
           },
         ]);
       }
 
-      setMessage(""); // 메시지 전송 후 인풋 초기화
+      setMessage("");
     }
   };
 
   const handleButtonClick = async (action, buttonText) => {
-    // 버튼 텍스트를 사용자 메시지로 추가
     setChatMessages((prev) => [
       ...prev,
       {
         id: `${action}-user-${Date.now()}`,
         sender: "user",
+        time: formatDate(new Date()),
         content: (
           <div className={styles.bubbleContainer_user}>
+            <p className={styles.timeStampUser}>{formatDate(new Date())}</p>
             <p className={styles.userText}>{buttonText}</p>
           </div>
         ),
       },
     ]);
 
-    // 챗봇 응답 추가
     const messageData = await getMessageData(action);
     if (messageData) {
       setChatMessages((prev) => [
@@ -176,17 +199,20 @@ export default function ChatBot() {
         {
           id: `${action}-${Date.now()}`,
           sender: messageData.sender,
+          time: formatDate(new Date()),
           content: (
-            <div>
-              <p className={styles.senderLabel}>KT 챗봇</p>
-              <div className={styles.bubbleContainer3}>
-                <p className={styles.yogoPlan_title}>{messageData.title}</p>
-                {/* HTML로 해석되도록 dangerouslySetInnerHTML 적용 */}
-                <p
-                  className={styles.yogoPlan_content}
-                  dangerouslySetInnerHTML={{ __html: messageData.content }}
-                ></p>
+            <div className={styles.bubbleContainer_bot}>
+              <div className={styles.botTextBox}>
+                <p className={styles.senderLabel}>KT 챗봇</p>
+                <div className={styles.bubbleContainer3}>
+                  <p className={styles.yogoPlan_title}>{messageData.title}</p>
+                  <p
+                    className={styles.yogoPlan_content}
+                    dangerouslySetInnerHTML={{ __html: messageData.content }}
+                  ></p>
+                </div>
               </div>
+              <p className={styles.timeStampBot}>{formatDate(new Date())}</p>
             </div>
           ),
         },
@@ -205,7 +231,7 @@ export default function ChatBot() {
                 msg.sender === "bot" ? styles.botMessage : styles.userMessage
               }
             >
-              {msg.content || msg.text}
+              {msg.content}
             </li>
           ))}
         </ul>
